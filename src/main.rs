3310b375b880bdf43fb556db9eebb66386748cd2
Nill Ringil <47075922+Nill-R@users.noreply.github.com>
2024-09-05 08:47:11 +0200

    Update main.rs
    Change default to my server(check.ingolonde.pw)

--- src/main.rs
+++ src/main.rs
@@ -168,7 +168,7 @@ struct Cli {
     user_agent: Option<String>,
 
     /// Base URL of the Healthchecks.io server to ping
-    #[clap(long, env="HEALTHCHECKS_BASE_URL", default_value="https://hc-ping.com")]
+    #[clap(long, env="HEALTHCHECKS_BASE_URL", default_value="https://check.ingolonde.pw")]
     base_url: String,
 
     /// The command to run
